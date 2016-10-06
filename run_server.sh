8bdd04db82a731bebaf9c9d79234d13cf37bd742
nikos <nikosglikis@gmail.com>
2016-10-06 21:36:08 +0300

    added --rm to the docker run commands.

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -ti -p 7981:7981 caffe:cpu  python server.py 7981
+docker run --rm -ti  -p 7981:7981 caffe:cpu  python server.py 7981
--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -ti caffe:cpu python ./classify_nsfw.py https://assets.entrepreneur.com/content/16x9/822/1389640367-young-female-entrepreneurs-advice-securing-funding.jpg
+docker run --rm -ti  caffe:cpu python ./classify_nsfw.py https://assets.entrepreneur.com/content/16x9/822/1389640367-young-female-entrepreneurs-advice-securing-funding.jpg
